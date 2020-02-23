1acdb614162a6a49dbd58224308cb51ea07bcc01
Janusz Augustyński <ocq@tutanota.com>
2020-02-23 15:14:11 +0100

    poprawki

--- main.c
+++ main.c
@@ -47,7 +47,10 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance
                     V( "M" );
                 E_text_Z_s0_P_copy_0( E_text_Z_s_P_copy_l( s, current_dir, l - 2 ), fd.cFileName );
                 object->data = s;
-                I font_id = E_wnd_Q_object_Z_font_M( window, 0, fd.cFileName );
+                Pc dir_utf8 = E_text_Z_s_Z_ansi_R_utf8( fd.cFileName );
+                if( !dir_utf8 )
+                    V( "E_text_Z_s_Z_ansi_R_utf8" );
+                I font_id = E_wnd_Q_object_Z_font_M( window, 0, dir_utf8 );
                 if( !~font_id )
                     V( "E_wnd_Q_object_Z_font_M" );
                 if( !~E_wnd_Q_object_I_add( window, dir_id, font_id ))
@@ -82,7 +85,10 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance
                     V( "M" );
                 E_text_Z_s0_P_copy_0( E_text_Z_s_P_copy_l( s, current_dir, l - 2 ), fd.cFileName );
                 object->data = s;
-                I font_id = E_wnd_Q_object_Z_font_M( window, 0, fd.cFileName );
+                Pc file_utf8 = E_text_Z_s_Z_ansi_R_utf8( fd.cFileName );
+                if( !file_utf8 )
+                    V( "E_text_Z_s_Z_ansi_R_utf8" );
+                I font_id = E_wnd_Q_object_Z_font_M( window, 0, file_utf8 );
                 if( !~font_id )
                     V( "E_wnd_Q_object_Z_font_M" );
                 if( !~E_wnd_Q_object_I_add( window, file_id, font_id ))
@@ -124,24 +130,23 @@ dir_click(
         l = parent_dir_ - (Pc)object->data + 3;
         dir = M(l);
         E_text_Z_s_P_copy_( dir, object->data, parent_dir_ );
-        dir[ l - 3 ] = '\\';
-        dir[ l - 2 ] = '*';
-        dir[ l - 1 ] = '\0';
     }else
     {   l = E_text_Z_s0_R_l( object->data ) + 3;
         dir = M(l);
         E_text_Z_s0_P_copy( dir, object->data );
-        dir[ l - 3 ] = '\\';
-        dir[ l - 2 ] = '*';
-        dir[ l - 1 ] = '\0';
     }
+    dir[ l - 3 ] = '\\';
+    dir[ l - 2 ] = '*';
+    dir[ l - 1 ] = '\0';
     WIN32_FIND_DATA fd;
     HANDLE find_handle = FindFirstFile( dir, &fd );
     if( find_handle == INVALID_HANDLE_VALUE )
-        V( "FindFirstFile" );
+    {   W(dir);
+        MessageBox( window->h, "FindFirstFile", "err", MB_OK );
+        return;
+    }
     for_each_rev( object_id, window->object, E_mem_Q_tab )
         E_wnd_Q_object_W( window, object_id );
-    SetWindowText( window->h, dir );
     I root_frame_id = E_wnd_Q_object_Z_frame_M(window);
     if( !~root_frame_id )
         V( "E_wnd_Q_object_Z_frame_M" );
@@ -163,10 +168,10 @@ dir_click(
                 V( "M" );
             E_text_Z_s0_P_copy_0( E_text_Z_s_P_copy_l( s, dir, l - 2 ), fd.cFileName );
             object->data = s;
-            Pc file_utf8 = E_text_Z_s_Z_ansi_R_utf8( fd.cFileName );
-            if( !file_utf8 )
+            Pc dir_utf8 = E_text_Z_s_Z_ansi_R_utf8( fd.cFileName );
+            if( !dir_utf8 )
                 V( "E_text_Z_s_Z_ansi_R_utf8" );
-            I font_id = E_wnd_Q_object_Z_font_M( window, 0, file_utf8 );
+            I font_id = E_wnd_Q_object_Z_font_M( window, 0, dir_utf8 );
             if( !~font_id )
                 V( "E_wnd_Q_object_Z_font_M" );
             if( !~E_wnd_Q_object_I_add( window, dir_id, font_id ))
@@ -218,6 +223,7 @@ dir_click(
     }
     if( !FindClose( find_handle ))
         V( "FindClose" );
+    SetWindowText( window->h, dir );
     W(dir);
     E_wnd_Q_object_I_lay(window);
     RECT rectangle =
@@ -232,8 +238,108 @@ file_click(
 , struct E_wnd_Q_object_Z *object
 , N8 button
 , N8 sequence
-){  N ret = ShellExecute( window->h, NULL, object->data, NULL, NULL, SW_SHOWNORMAL );
+){  N ret = (N)ShellExecute( window->h, 0, object->data, 0, 0, SW_SHOWNORMAL );
     if( ret == SE_ERR_NOASSOC )
         MessageBox( window->h, "not associated", "err", MB_OK );
+    else if( ret == SE_ERR_FNF )
+    {   Pc file = E_text_Z_s0_R_search_last_c( object->data, '\\' ) + 1;
+        N l = file - (Pc)object->data + 2;
+        Pc dir = M(l);
+        E_text_Z_s_P_copy_( dir, object->data, file );
+        dir[ l - 2 ] = '*';
+        dir[ l - 1 ] = '\0';
+        WIN32_FIND_DATA fd;
+        HANDLE find_handle = FindFirstFile( dir, &fd );
+        if( find_handle == INVALID_HANDLE_VALUE )
+        {   W(dir);
+            MessageBox( window->h, "FindFirstFile", "err", MB_OK );
+            return;
+        }
+        for_each_rev( object_id, window->object, E_mem_Q_tab )
+            E_wnd_Q_object_W( window, object_id );
+        I root_frame_id = E_wnd_Q_object_Z_frame_M(window);
+        if( !~root_frame_id )
+            V( "E_wnd_Q_object_Z_frame_M" );
+        O{ if( fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY )
+            {   I frame_id = E_wnd_Q_object_Z_frame_M(window);
+                if( !~frame_id )
+                    V( "E_wnd_Q_object_Z_frame_M" );
+                if( !~E_wnd_Q_object_I_add( window, root_frame_id, frame_id ))
+                    V( "E_wnd_Q_object_I_add" );
+                I dir_id = E_wnd_Q_object_Z_button_M(window);
+                if( !~dir_id )
+                    V( "E_wnd_Q_object_Z_button_M" );
+                if( !~E_wnd_Q_object_I_add( window, frame_id, dir_id ))
+                    V( "E_wnd_Q_object_I_add" );
+                struct E_wnd_Q_object_Z *object = E_mem_Q_tab_R( window->object, dir_id );
+                object->click = dir_click;
+                Pc s = M( l - 2 + E_text_Z_s0_R_l( fd.cFileName ) + 1 );
+                if( !s )
+                    V( "M" );
+                E_text_Z_s0_P_copy_0( E_text_Z_s_P_copy_l( s, dir, l - 2 ), fd.cFileName );
+                object->data = s;
+                Pc dir_utf8 = E_text_Z_s_Z_ansi_R_utf8( fd.cFileName );
+                if( !dir_utf8 )
+                    V( "E_text_Z_s_Z_ansi_R_utf8" );
+                I font_id = E_wnd_Q_object_Z_font_M( window, 0, dir_utf8 );
+                if( !~font_id )
+                    V( "E_wnd_Q_object_Z_font_M" );
+                if( !~E_wnd_Q_object_I_add( window, dir_id, font_id ))
+                    V( "E_wnd_Q_object_I_add" );
+            }
+            if( !FindNextFile( find_handle, &fd ))
+            {   if( GetLastError() == ERROR_NO_MORE_FILES )
+                    break;
+                V( "FindNextFile" );
+            }
+        }
+        if( !FindClose( find_handle ))
+            V( "FindClose" );
+        find_handle = FindFirstFile( dir, &fd );
+        if( find_handle == INVALID_HANDLE_VALUE )
+            V( "FindFirstFile" );
+        O{ if( !( fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ))
+            {   I frame_id = E_wnd_Q_object_Z_frame_M(window);
+                if( !~frame_id )
+                    V( "E_wnd_Q_object_Z_frame_M" );
+                if( !~E_wnd_Q_object_I_add( window, root_frame_id, frame_id ))
+                    V( "E_wnd_Q_object_I_add" );
+                I file_id = E_wnd_Q_object_Z_button_M(window);
+                if( !~file_id )
+                    V( "E_wnd_Q_object_Z_button_M" );
+                if( !~E_wnd_Q_object_I_add( window, frame_id, file_id ))
+                    V( "E_wnd_Q_object_I_add" );
+                struct E_wnd_Q_object_Z *object = E_mem_Q_tab_R( window->object, file_id );
+                object->click = file_click;
+                Pc s = M( l - 2 + E_text_Z_s0_R_l( fd.cFileName ) + 1 );
+                if( !s )
+                    V( "M" );
+                E_text_Z_s0_P_copy_0( E_text_Z_s_P_copy_l( s, dir, l - 2 ), fd.cFileName );
+                object->data = s;
+                Pc file_utf8 = E_text_Z_s_Z_ansi_R_utf8( fd.cFileName );
+                if( !file_utf8 )
+                    V( "E_text_Z_s_Z_ansi_R_utf8" );
+                I font_id = E_wnd_Q_object_Z_font_M( window, 0, file_utf8 );
+                if( !~font_id )
+                    V( "E_wnd_Q_object_Z_font_M" );
+                if( !~E_wnd_Q_object_I_add( window, file_id, font_id ))
+                    V( "E_wnd_Q_object_I_add" );
+            }
+            if( !FindNextFile( find_handle, &fd ))
+            {   if( GetLastError() == ERROR_NO_MORE_FILES )
+                    break;
+                V( "FindNextFile" );
+            }
+        }
+        if( !FindClose( find_handle ))
+            V( "FindClose" );
+        W(dir);
+        E_wnd_Q_object_I_lay(window);
+        RECT rectangle =
+        { 0, 0
+        , window->width, window->height
+        };
+        InvalidateRect( window->h, &rectangle, TRUE );
+    }
 }
 
