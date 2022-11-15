c70fd3d04dd7938903dfc390714015f93226a04a
Janusz Augustyński <ocq@tutanota.com>
2022-11-15 11:37:44 +0100

    Work in progress

--- module/base/dllmain.c
+++ module/base/dllmain.c
@@ -1,30 +1,24 @@
-DLLIMPORT void HelloWorld()
-{
-	MessageBox(0,"Hello World from DLL!\n","Hi",MB_ICONINFORMATION);
-}
-
-BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
-{
-	switch(fdwReason)
-	{
-		case DLL_PROCESS_ATTACH:
-		{
-			break;
-		}
-		case DLL_PROCESS_DETACH:
-		{
-			break;
-		}
-		case DLL_THREAD_ATTACH:
-		{
-			break;
-		}
-		case DLL_THREAD_DETACH:
-		{
-			break;
-		}
+WINAPI BOOL
+DllMain( HINSTANCE hinstDLL
+, DWORD fdwReason
+, LPVOID lpvReserved
+){  switch(fdwReason)
+	{ case DLL_PROCESS_ATTACH:
+            {
+                break;
+            }
+	  case DLL_PROCESS_DETACH:
+            {
+                break;
+            }
+	  case DLL_THREAD_ATTACH:
+            {
+                break;
+            }
+	  case DLL_THREAD_DETACH:
+            {
+                break;
+            }
 	}
-	
-	/* Return TRUE on success, FALSE on failure */
 	return TRUE;
 }
